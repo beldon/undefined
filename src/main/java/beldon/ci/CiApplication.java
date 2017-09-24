2858c33d00009f59f299ce72a458c457b4c741bc
beldon <1047325205@qq.com>
2017-09-24 11:43:13 +0800

    dd

--- src/main/java/beldon/ci/CiApplication.java
+++ src/main/java/beldon/ci/CiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("dd");
 		SpringApplication.run(CiApplication.class, args);
 	}
 }

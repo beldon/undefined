5fa03e158e028992de46a17259380ff397794801
beldon <1047325205@qq.com>
2017-09-24 12:50:05 +0800

    dd

--- src/main/java/beldon/ci/CiApplication.java
+++ src/main/java/beldon/ci/CiApplication.java
@@ -8,6 +8,7 @@ public class CiApplication {
 
 	public static void main(String[] args) {
 		System.out.println("dd");
+		System.out.println("sss");
 		SpringApplication.run(CiApplication.class, args);
 	}
 }
